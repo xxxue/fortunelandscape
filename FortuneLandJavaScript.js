72b8f8d1fc93e5684013037519b7b61d743c7492
xxxue <rzong.lin@gmail.com>
2025-08-22 22:05:32 -0400

    Add files via upload

---
+++ FortuneLandJavaScript.js
@@ -0,0 +1,16 @@
+const gallery = document.querySelectorAll('.portfolio img');
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = document.getElementById('lightbox-img');
+
+  // 点击缩略图 -> 显示大图
+  gallery.forEach(img => {
+    img.addEventListener('click', () => {
+      lightbox.style.display = 'flex';
+      lightboxImg.src = img.src; // 显示被点击的图片
+    });
+  });
+
+  // 点击背景 -> 关闭大图
+  lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
