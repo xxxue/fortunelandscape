d00ff7cfd590630ab99b44a22512d2a266d831e9
xxxue <rzong.lin@gmail.com>
2025-09-17 21:38:06 -0400

    Update FortuneLandJavaScript.js

--- FortuneLandJavaScript.js
+++ FortuneLandJavaScript.js
@@ -1,22 +1,19 @@
-const gallery = document.querySelectorAll('.portfolio img');
+const frontDoor = document.querySelectorAll('.frontDoorImages img');
   const lightbox = document.getElementById('lightbox');
   const lightboxImg = document.getElementById('lightbox-img');
 
-  // 点击缩略图 -> 显示大图
-  gallery.forEach(img => {
+  frontDoor.forEach(img => {
     img.addEventListener('click', () => {
       lightbox.style.display = 'flex';
-      lightboxImg.src = img.src; // 显示被点击的图片
+      lightboxImg.src = img.src; 
     });
   });
 
-  // 点击背景 -> 关闭大图
+
   lightbox.addEventListener('click', () => {
     lightbox.style.display = 'none';
-
   });
 
-  function showMore() {
-    document.getElementById("morePortfolio").classList.remove("hidden");
-    document.querySelector(".seeMore").style.display = "none"; 
-  }
+
+  
+  
